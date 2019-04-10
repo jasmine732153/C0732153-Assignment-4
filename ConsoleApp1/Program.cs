afddf80a2723a4bf990df55eb2d92e7fd3bf114b
visualstudio <visualstudio@122-PC39>
2019-04-10 08:37:29 -0400

    second commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
-            Console.WriteLine(kajal);
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+        public void Run()
+        { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+    {
+            using (StreamReader file = new StreamReader(@"U:\Users\732153\New folder"))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+            }
+    }
     }
 }
